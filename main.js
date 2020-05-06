19a98b46a5f893e0a6f5cd653cc301c03c9c5533
Ander Punnar <ander@kvlt.ee>
2020-05-06 19:53:55 +0300

    more prettier

--- main.js
+++ main.js
@@ -3,62 +3,62 @@ const contextMenu = require('electron-context-menu');
 const path = require('path');
 
 function run() {
-  let url = process.argv[process.argv.length - 1];
+    let url = process.argv[process.argv.length - 1];
 
-  if (!url.match(/^https?:\/\//)) {
-    console.log('RUNWEB MISSING URL');
-    process.exit();
-  }
-
-  let hostname = new URL(url).hostname;
-  console.log('RUNWEB HOSTNAME: ' + hostname);
-
-  let win = new BrowserWindow({
-    minWidth: 800,
-    minHeight: 600,
-    backgroundColor: '#fff',
-    autoHideMenuBar: true,
-    webPreferences: {
-      partition: 'persist:' + hostname,
-      preload: path.join(__dirname, 'preload.js')
+    if (!url.match(/^https?:\/\//)) {
+        console.log('RUNWEB MISSING URL');
+        process.exit();
     }
-  });
 
-  console.log('RUNWEB LOAD: ' + url);
-  win.loadURL(url);
+    let hostname = new URL(url).hostname;
+    console.log('RUNWEB HOSTNAME: ' + hostname);
 
-  contextMenu({
-    prepend: (defaultActions, params, browserWindow) => [
-      {
-        label: 'Reload',
-        click: () => {
-          win.reload();
+    let win = new BrowserWindow({
+        minWidth: 800,
+        minHeight: 600,
+        backgroundColor: '#fff',
+        autoHideMenuBar: true,
+        webPreferences: {
+            partition: 'persist:' + hostname,
+            preload: path.join(__dirname, 'preload.js')
         }
-      }
-    ]
-  });
+    });
 
-  win.webContents.on('page-title-updated', function (event, title) {
-    event.preventDefault();
-    title = '[runweb@' + hostname + '] ' + title;
-    console.log('RUNWEB TITLE: ' + title);
-    win.setTitle(title);
-  });
+    console.log('RUNWEB LOAD: ' + url);
+    win.loadURL(url);
 
-  win.webContents.on('new-window', function (event, url) {
-    event.preventDefault();
-    console.log('RUNWEB EXTERNAL: ' + url);
-    shell.openExternal(url);
-  });
+    contextMenu({
+        prepend: (defaultActions, params, browserWindow) => [
+            {
+                label: 'Reload',
+                click: () => {
+                    win.reload();
+                }
+            }
+        ]
+    });
 
-  win.on('close', function () {
-    win = null;
-  });
+    win.webContents.on('page-title-updated', function (event, title) {
+        event.preventDefault();
+        title = '[runweb@' + hostname + '] ' + title;
+        console.log('RUNWEB TITLE: ' + title);
+        win.setTitle(title);
+    });
 
-  ipcMain.on('flashFrame', (event) => {
-    console.log('RUNWEB NOTIFICATION');
-    win.flashFrame(true);
-  });
+    win.webContents.on('new-window', function (event, url) {
+        event.preventDefault();
+        console.log('RUNWEB EXTERNAL: ' + url);
+        shell.openExternal(url);
+    });
+
+    win.on('close', function () {
+        win = null;
+    });
+
+    ipcMain.on('flashFrame', (event) => {
+        console.log('RUNWEB NOTIFICATION');
+        win.flashFrame(true);
+    });
 }
 
 app.on('ready', run);
--- preload.js
+++ preload.js
@@ -1,10 +1,10 @@
 const { ipcRenderer } = require('electron');
 
 class runwebNotification extends Notification {
-  constructor(title, options) {
-    super(title, options);
-    ipcRenderer.send('flashFrame');
-  }
+    constructor(title, options) {
+        super(title, options);
+        ipcRenderer.send('flashFrame');
+    }
 }
 
 window.Notification = runwebNotification;
